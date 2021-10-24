b34480b85dc884bc2e48df3cbf39901c75115524
Cassiana <lima.cglima@gmail.com>
2021-10-24 20:31:24 -0300

    Estrutura sequencial https://wiki.python.org.br/EstruturaSequencial
    Resolução do exercício 07

---
+++ estrutura_sequencial/exercicio_07.py
@@ -0,0 +1,7 @@
+""" 
+7. Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+"""
+lado_quadrado = float(input("Digite o comprimento de um lado do quadrado: "))
+area_quadrado = lado_quadrado * 2
+dobro_area = 2 * area_quadrado
+print(f"A área do quadrado é igual a {area_quadrado}m2 e o dobro desta área é igual a {dobro_area}")
