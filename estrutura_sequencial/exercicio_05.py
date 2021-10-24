0c4193626d6dbd081a66da2ec0517b5fc1dc9020
Cassiana <lima.cglima@gmail.com>
2021-10-24 20:15:35 -0300

    Estrutura sequencial https://wiki.python.org.br/EstruturaSequencial
    Resolução do exercício 05

---
+++ estrutura_sequencial/exercicio_05.py
@@ -0,0 +1,6 @@
+""" 
+5. Faça um Programa que converta metros para centímetros.
+"""
+medida_em_metro = float(input("Digite um comprimento em 'metros' para converter para 'centimetros': "))
+metro_para_centimetro = medida_em_metro * 100
+print(f"{medida_em_metro} m equivale a {metro_para_centimetro} cm")
