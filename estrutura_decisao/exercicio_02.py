7d3ddeb31d2468d2612d7b73eceadb463fb1b8d5
Cassiana <lima.cglima@gmail.com>
2021-10-25 22:38:05 -0300

    Estrutura de decisão https://wiki.python.org.br/EstruturaDeDecisao
    Resolução do exercício 03

--- estrutura_decisao/exercicio_02.py
+++ estrutura_decisao/exercicio_02.py
@@ -1,5 +1,5 @@
 """
-Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+2. Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
 """
 numero = int(input("Digite um número: "))
 if numero > 0:
